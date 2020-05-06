5eab62cf50e60c16f8b8989494d7867f2a249ca5
William Trammell <Willt125@users.noreply.github.com>
2020-05-05 20:15:11 -0400

    Update MGD_Decode2400_4800.py

--- MGD_Decode2400_4800.py
+++ MGD_Decode2400_4800.py
@@ -1,4 +1,9 @@
-def MGD_Decode2400_4800(inArray):
+def MGD_Decode2400_4800(inArray,interleave_length,inter_len,Bd,BdType):
+	data_map = []
+	if interleave_length == 'S':
+		setlen = 45
+	else:
+		setlen = 360
 	# Modified Gray Decodes the input data into symbols ready to scramble
 	mgd = []
 	for i in range(len(inArray)): # converts binary to Gray
@@ -19,24 +24,44 @@ def MGD_Decode2400_4800(inArray):
 		elif inArray[i] == '111':
 			mgd.append('101')
 	data_map = []
+	
 	for i in range(len(mgd)): # converts Gray to symbols
-		if i % (32*45) == 0: #use exceptional set
-			if mgd[i] == '000':
-				data_map.append('0')
-			elif mgd[i] == '001':
-				data_map.append('1')
-			elif mgd[i] == '010':
-				data_map.append('2')
-			elif mgd[i] == '011':
-				data_map.append('3')
-			if mgd[i] == '100':
-				data_map.append('4')
-			elif mgd[i] == '101':
-				data_map.append('5')
-			elif mgd[i] == '110':
-				data_map.append('6')
-			elif mgd[i] == '111':
-				data_map.append('7')
+		if mgd[i] == '000':
+			data_map.append('0')
+		elif mgd[i] == '001':
+			data_map.append('1')
+		elif mgd[i] == '010':
+			data_map.append('2')
+		elif mgd[i] == '011':
+			data_map.append('3')
+		elif mgd[i] == '100':
+			data_map.append('4')
+		elif mgd[i] == '101':
+			data_map.append('5')
+		elif mgd[i] == '110':
+			data_map.append('6')
+		elif mgd[i] == '111':
+			data_map.append('7')
+		if ((i % inter_len) == 0) and (i != 0):
+			if Bd == 2400 and BdType == 'Data':
+				if interleave_length == 'S':
+					data_map.extend('0'*1440)
+					data_map.extend('0044440000004444'*2)
+				if interleave_length == 'L':
+					data_map.extend('0'*11520)
+					data_map.extend('0000444400004444'*2)
+			if Bd == 2400 and BdType == 'Voice':
+				if interleave_length == 'S':
+					data_map.extend('0'*1440)
+					data_map.extend('0440400404404004'*2)
+				if interleave_length == 'L':
+					raise ValueError("Only Short interleave is allowed for Secure Voice")
+			if Bd == 4800:
+				if interleave_length == 'S':
+					data_map.extend('0'*1440)
+					data_map.extend('0440400400444400'*2)
+				if interleave_length == 'L':
+					raise ValueError("Only Short interleave is supported for 4800Bd")
 	data_map2 = []
 	for i in range(len(data_map)):
 		for char in data_map[i]:
