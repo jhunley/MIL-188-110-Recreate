eb321e5eb83849ef6e793d75b6b829085a47de4e
Grog, the Python Man <Willt125@users.noreply.github.com>
2021-01-01 20:51:49 -0500

    Starting from scratch.
    Added the preamble generation function.

--- TotalEncode.py
+++ TotalEncode.py
@@ -1,210 +1,125 @@
-def TotalEncode(instring,Bd=75,BdType='Data',interleave_length='S'):
-	if Bd == 75:
-		if interleave_length == 'S':
-			inter_len = 90
-			numrows = 10
-			numcols = 9
-			rowinc = 7
-			rowincmod = 10
-			coldec = 7
-			fetch_num = 2
-			indata = EncodeInputText(instring,inter_len)
-			fec_out = FECEncodeBits(indata,Bd)
-			inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-			datamap = MGD_Decode75(inter_out)
-			scrambld = ScrambleBits(datamap)
-			message = messageCompose(scramsync75s,scrambld)
-			outsound = convertToTones(message)
-		elif interleave_length == 'L':
-			inter_len = 720
-			numrows = 20
-			numcols = 36
-			rowinc = 7
-			rowincmod = 20
-			coldec = 7
-			fetch_num = 2
-			indata = EncodeInputText(instring,inter_len)
-			fec_out = FECEncodeBits(indata,Bd)
-			inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-			datamap = MGD_Decode75(inter_out)
-			scrambld = ScrambleBits(datamap)
-			message = messageCompose(scramsync75l,scrambld)
-			outsound = convertToTones(message)
-	if Bd == 150:
-		if interleave_length == 'S':
-			inter_len = 720
-			numrows = 40
-			numcols = 18
-			rowinc = 9
-			rowincmod = 40
-			coldec = 17
-			fetch_num = 1
-			indata = EncodeInputText(instring,inter_len)
-			fec_out = FECEncodeBits(indata,Bd)
-			inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-			datamap = MGD_Decode150_600(inter_out,inter_len,Bd)
-			scrambld = ScrambleBits(datamap)
-			message = messageCompose(scramsync150s,scrambld)
-			outsound = convertToTones(message)
-		elif interleave_length == 'L':
-			inter_len = 5760
-			numrows = 40
-			numcols = 144
-			rowinc = 9
-			rowincmod = 40
-			coldec = 17
-			fetch_num = 1
-			indata = EncodeInputText(instring,inter_len)
-			fec_out = FECEncodeBits(indata,Bd)
-			inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-			datamap = MGD_Decode150_600(inter_out,inter_len,Bd)
-			scrambld = ScrambleBits(datamap)
-			message = messageCompose(scramsync150l,scrambld)
-			outsound = convertToTones(message)
-	if Bd == 300:
-		if interleave_length == 'S':
-			inter_len = 720
-			numrows = 40
-			numcols = 18
-			rowinc = 9
-			rowincmod = 40
-			coldec = 17
-			fetch_num = 1
-			indata = EncodeInputText(instring,inter_len)
-			fec_out = FECEncodeBits(indata,Bd)
-			inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-			datamap = MGD_Decode150_600(inter_out,inter_len,Bd)
-			scrambld = ScrambleBits(datamap)
-			message = messageCompose(scramsync300s,scrambld)
-			outsound = convertToTones(message)
-		elif interleave_length == 'L':
-			inter_len = 5760
-			numrows = 40
-			numcols = 144
-			rowinc = 9
-			rowincmod = 40
-			coldec = 17
-			fetch_num = 1
-			indata = EncodeInputText(instring,inter_len)
-			fec_out = FECEncodeBits(indata,Bd)
-			inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-			datamap =MGD_Decode150_600(inter_out,inter_len,Bd)
-			scrambld = ScrambleBits(datamap)
-			message = messageCompose(scramsync300l,scrambld)
-			outsound = convertToTones(message)
-	if Bd == 600:
-		if interleave_length == 'S':
-			inter_len = 720
-			numrows = 40
-			numcols = 18
-			rowinc = 9
-			rowincmod = 40
-			coldec = 17
-			fetch_num = 1
-			indata = EncodeInputText(instring,inter_len)
-			fec_out = FECEncodeBits(indata,Bd)
-			inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-			datamap =MGD_Decode150_600(inter_out,inter_len,Bd)
-			scrambld = ScrambleBits(datamap)
-			message = messageCompose(scramsync600s,scrambld)
-			outsound = convertToTones(message)
-		elif interleave_length == 'L':
-			inter_len = 5760
-			numrows = 40
-			numcols = 144
-			rowinc = 9
-			rowincmod = 40
-			coldec = 17
-			fetch_num = 1
-			indata = EncodeInputText(instring,inter_len)
-			fec_out = FECEncodeBits(indata,Bd)
-			inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-			datamap = MGD_Decode150_600(inter_out,inter_len,Bd)
-			scrambld = ScrambleBits(datamap)
-			message = messageCompose(scramsync600l,scrambld)
-			outsound = convertToTones(message)
-	if Bd == 1200:
-		if interleave_length == 'S':
-			inter_len = 1440
-			numrows = 40
-			numcols = 36
-			rowinc = 9
-			rowincmod = 40
-			coldec = 17
-			fetch_num = 2
-			indata = EncodeInputText(instring,inter_len)
-			fec_out = FECEncodeBits(indata,Bd)
-			inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-			datamap = MGD_Decode1200(inter_out,interleave_length)
-			scrambld = ScrambleBits(datamap)
-			message = messageCompose(scramsync1200s,scrambld)
-			outsound = convertToTones(message)
-		elif interleave_length == 'L':
-			inter_len = 11520
-			numrows = 40
-			numcols = 288
-			rowinc = 9
-			rowincmod = 40
-			coldec = 17
-			fetch_num = 1
-			indata = EncodeInputText(instring,inter_len)
-			fec_out = FECEncodeBits(indata,Bd)
-			inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-			datamap = MGD_Decode1200(inter_out,interleave_length)
-			scrambld = ScrambleBits(datamap)
-			message = messageCompose(scramsync1200l,scrambld)
-			outsound = convertToTones(message)
-	if Bd == 2400 and BdType == 'Data':
-		if interleave_length == 'S':
-			inter_len = 2880
-			numrows = 40
-			numcols = 72
-			rowinc = 9
-			rowincmod = 40
-			coldec = 17
-			fetch_num = 3
-			indata = EncodeInputText(instring,inter_len)
-			fec_out = FECEncodeBits(indata,Bd)
-			inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-			datamap = MGD_Decode2400_4800(inter_out,interleave_length,inter_len,Bd,BdType)
-			scrambld = ScrambleBits(datamap)
-			message = messageCompose(scramsync2400sd,scrambld)
-			outsound = convertToTones(message)
-		elif interleave_length == 'L':
-			inter_len = 23040
-			numrows = 40
-			numcols = 576
-			rowinc = 9
-			rowincmod = 40
-			coldec = 17
-			fetch_num = 3
-			indata = EncodeInputText(instring,inter_len)
-			fec_out = FECEncodeBits(indata,Bd)
-			inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-			datamap = MGD_Decode2400_4800(inter_out,interleave_length,inter_len,Bd,BdType)
-			scrambld = ScrambleBits(datamap)
-			message = messageCompose(scramsync2400ld,scrambld)
-			outsound = convertToTones(message)
-	if Bd == 2400 and BdType == 'Voice':
-		inter_len = 2880
-		numrows = 40
-		numcols = 72
-		rowinc = 9
-		rowincmod = 40
-		coldec = 17
-		fetch_num = 3
-		indata = EncodeInputText(instring,inter_len)
-		fec_out = FECEncodeBits(indata,Bd)
-		inter_out = InterleaveBits(fec_out,inter_len,numrows,numcols,rowinc,rowincmod,coldec,fetch_num)
-		datamap = MGD_Decode2400_4800(inter_out,interleave_length,inter_len,Bd,BdType)
-		scrambld = ScrambleBits(datamap)
-		message = messageCompose(scramsync2400sv,scrambld)
-		outsound = convertToTones(message)
-	if Bd == 4800:
-		inter_len = len(instring)
-		indata = EncodeInputText(instring,inter_len)
-		datamap = MGD_Decode2400_4800(inter_out,interleave_length,inter_len,Bd,BdType)
-		scrambld = ScrambleBits(datamap)
-		message = messageCompose(scramsync4800,scrambld)
-		outsound = convertToTones(message)
-	return outsound
+import numpy as np
+import scipy.io.wavfile as wavfile
+
+x = np.arange(160)
+x = x / 384000
+f_c = 1800
+tone0 = np.cos(2*np.pi*f_c*x)
+tone1 = np.cos(2*np.pi*f_c*x+45)
+tone2 = np.cos(2*np.pi*f_c*x+90)
+tone3 = np.cos(2*np.pi*f_c*x+135)
+tone4 = np.cos(2*np.pi*f_c*x+180)
+tone5 = np.cos(2*np.pi*f_c*x+225)
+tone6 = np.cos(2*np.pi*f_c*x+270)
+tone7 = np.cos(2*np.pi*f_c*x+315)
+
+def PreambleGenerate(data_array, bps, interleave_len, dtyp = "Data"):
+	if interleave_len == "Z" or interleave_len == "S":
+		count = 2
+		while count >= 0:
+			data_array = np.append(data_array, [0b000,0b001,0b011,0b000,0b001,0b011,0b001,0b010,0b000])
+			if bps == 75:
+				data_array = np.append(data_array, [0b111,0b101])
+			elif bps == 150:
+				data_array = np.append(data_array, [0b111,0b100])
+			elif bps == 300:
+				data_array = np.append(data_array, [0b110,0b111])
+			elif bps == 600:
+				data_array = np.append(data_array, [0b110,0b110])
+			elif bps == 1200:
+				data_array = np.append(data_array, [0b110,0b101])
+			elif bps == 2400 and dtyp == "Data":
+				data_array = np.append(data_array, [0b110,0b100])
+			elif bps == 2400 and dtyp == "Voice":
+				data_array = np.append(data_array, [0b111,0b111])
+			elif bps == 4800:
+				data_array = np.append(data_array, [0b111,0b110])
+			else:
+				raise Exception("Invalid input.")
+			temp = bin(count)[2:].zfill(6)
+			C1 = temp[:2]
+			C2 = temp[2:4]
+			C3 = temp[4:]
+			if C1 == '00':
+				C1 = 0b100
+			elif C1 == '10':
+				C1 = 0b101
+			elif C1 == '01':
+				C1 = 0b110
+			elif C1 == '11':
+				C1 = 0b111
+			if C2 == '00':
+				C2 = 0b100
+			elif C2 == '10':
+				C2 = 0b101
+			elif C2 == '01':
+				C2 = 0b110
+			elif C2 == '11':
+				C2 = 0b111
+			if C3 == '00':
+				C3 = 0b100
+			elif C3 == '10':
+				C3 = 0b101
+			elif C3 == '01':
+				C3 = 0b110
+			elif C3 == '11':
+				C3 = 0b111
+			data_array = np.append(data_array, [C1, C2, C3, 0b000])
+			count -= 1
+		return data_array
+	elif interleave_len == "L":
+		count = 23
+		while count >= 0:
+			data_array = np.append(data_array, [0b000,0b001,0b011,0b000,0b001,0b011,0b001,0b010,0b000])
+			if bps == 75:
+				data_array = np.append(data_array, [0b111,0b101])
+			elif bps == 150:
+				data_array = np.append(data_array, [0b111,0b100])
+			elif bps == 300:
+				data_array = np.append(data_array, [0b110,0b111])
+			elif bps == 600:
+				data_array = np.append(data_array, [0b110,0b110])
+			elif bps == 1200:
+				data_array = np.append(data_array, [0b110,0b101])
+			elif bps == 2400 and dtyp == "Data":
+				data_array = np.append(data_array, [0b110,0b100])
+			elif bps == 2400 and dtyp == "Voice":
+				data_array = np.append(data_array, [0b111,0b111])
+			elif bps == 4800:
+				data_array = np.append(data_array, [0b111,0b110])
+			else:
+				raise RuntimeError("Invalid input.")
+			temp = bin(count)[2:].zfill(6)
+			C1 = temp[:2]
+			C2 = temp[2:4]
+			C3 = temp[4:]
+			if C1 == '00':
+				C1 = 0b100
+			elif C1 == '10':
+				C1 = 0b101
+			elif C1 == '01':
+				C1 = 0b110
+			elif C1 == '11':
+				C1 = 0b111
+			if C2 == '00':
+				C2 = 0b100
+			elif C2 == '10':
+				C2 = 0b101
+			elif C2 == '01':
+				C2 = 0b110
+			elif C2 == '11':
+				C2 = 0b111
+			if C3 == '00':
+				C3 = 0b100
+			elif C3 == '10':
+				C3 = 0b101
+			elif C3 == '01':
+				C3 = 0b110
+			elif C3 == '11':
+				C3 = 0b111
+			data_array = np.append(data_array, [C1, C2, C3, 0b000])
+			count -= 1
+		return data_array
+	else:
+		raise RuntimeError("Invalid input.")
+
