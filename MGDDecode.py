f9277988447cd27915fa01c767a8cef9a79e7011
Grog, the Python Man <Willt125@users.noreply.github.com>
2021-01-04 14:31:59 -0500

    Update MGDDecode.py

--- MGDDecode.py
+++ MGDDecode.py
@@ -1,1 +1,16 @@
-
+def MGD_Decode(indata, Bd):
+    
+    if (Bd == 75 or Bd == 1200):
+        group = 2
+        grouped = np.zeros([indata.shape[0]//2,])
+        
+    elif (Bd == 150 or Bd == 300 or Bd == 600):
+        group = 1
+        out = np.zeros(indata.shape)
+    elif (Bd == 2400 or Bd == 4800):
+        group = 3
+        out = np.zeros([indata.shape[0]//3,])
+    else:
+        raise RuntimeError("Invalid input.")
+    
+    
