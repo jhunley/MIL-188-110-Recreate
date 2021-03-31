32a662e99ad17bc3067aa54a19bcfd270eadeec6
Grog, the Python Man <Willt125@users.noreply.github.com>
2021-03-31 17:55:36 -0400

    Added comments

--- interleaveBits.h
+++ interleaveBits.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include "Types.h"
 
+// I plan on using this as part of a mode selector
 enum interleave_len
 {
 	_zero,
@@ -11,9 +12,16 @@ enum interleave_len
 	_long
 };
 
-interleave_len length = _short;
+/*
+	interleave_data() does all the management for interleave_chunk(), which does all the dirty work interleaving the input stream.
 
-void interleave_data(std::vector<byte>* datastream, size_t baud, interleave_len chunklen);
-void interleave_chunk(std::vector<byte>* chunk, size_t numrows, size_t numcols);
+	The interleaver works thus:
+	you pass in the input stream (and bit size) and mode parameters to interleave_data(), which sets up the size and interleave pattern
+	used by interleave_chunk().
+
+	They then go back and forth slicing the input stream, and in-place interleaving it. This module is parallelizable, probably embarassingly so.
+*/
+void interleave_data(std::vector<byte>* datastream, size_t baud, interleave_len chunklen, size_t bitlen);
+void interleave_chunk(std::vector<byte>* chunk, size_t numrows, size_t numcols, byte offset);
 
 #endif
