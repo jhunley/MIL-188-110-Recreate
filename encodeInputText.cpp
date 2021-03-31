d1141b33f7967de5778a881f984bf1a5ce51232c
Grog, the Python Man <Willt125@users.noreply.github.com>
2021-03-30 20:39:52 -0400

    Update encodeInputText.cpp

--- encodeInputText.cpp
+++ encodeInputText.cpp
@@ -4,21 +4,23 @@
 #include "Types.h"
 #include "encodeInputText.h"
 
-void encodeInputText(std::vector<byte>* datastream, size_t inter_len)
+size_t encodeInputText(std::vector<byte>* datastream, size_t inter_len)
 {
 	size_t arlen = datastream->size();
+	size_t bitlen = arlen * 8 + 320 + 144;
 
 	size_t additionalEl = 0;
 
-	while ((arlen + 40 + 144 + additionalEl) % inter_len != 0)
+	while ((bitlen + additionalEl) % inter_len != 0)
 	{
 		additionalEl++;
 	}
-	size_t outlen = (arlen + 40 + 144 + (additionalEl / 8)) * sizeof(byte);
+	size_t outlen = ((bitlen / 8) + ((bitlen % 8 != 0) ? 1 : 0) + (additionalEl / 8) + ((additionalEl % 8 != 0) ? 1 : 0)) * sizeof(byte);
+	bitlen += additionalEl;
 
 	datastream->insert(datastream->end(), &EOM[0], &EOM[39]);
 
 	datastream->resize(outlen);
 
-	return;
+	return bitlen;
 }
