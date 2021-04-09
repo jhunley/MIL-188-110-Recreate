4716ab94714687565d9f46981ac66eaac12393aa
Grog, the Python Man <Willt125@users.noreply.github.com>
2021-04-09 15:48:16 -0400

    RELEASE 0.01
    Made it faster!

--- ConvertToTones.py
+++ ConvertToTones.py
@@ -1,21 +1,23 @@
 def convertToTones(message):
-	outsound = np.array([None], dtype=np.float32)
-	for i in range(len(message)):
-		if message[i] == 0:
-			outsound = np.concatenate([outsound, tone0])
-		elif message[i] == 1:
-			outsound = np.concatenate([outsound, tone1])
-		elif message[i] == 2:
-			outsound = np.concatenate([outsound, tone2])
-		elif message[i] == 3:
-			outsound = np.concatenate([outsound, tone3])
-		elif message[i] == 4:
-			outsound = np.concatenate([outsound, tone4])
-		elif message[i] == 5:
-			outsound = np.concatenate([outsound, tone5])
-		elif message[i] == 6:
-			outsound = np.concatenate([outsound, tone6])
-		elif message[i] == 7:
-			outsound = np.concatenate([outsound, tone7])
-	outsound = np.delete(outsound, 0)
-	return outsound
+	# Ah, finally, the last step (almost). This is dead simple.
+	# Had to do it this way because numpy append and concatenate is SO SLOOOW
+        outsound = np.empty(len(message) * 160, dtype=np.float32)
+        _len = 160
+        for i in range(len(message)):
+                if message[i] == 0:
+                        outsound[i*_len:i*_len+_len] = tone0
+                elif message[i] == 1:
+                        outsound[i*_len:i*_len+_len] = tone1
+                elif message[i] == 2:
+                        outsound[i*_len:i*_len+_len] = tone2
+                elif message[i] == 3:
+                        outsound[i*_len:i*_len+_len] = tone3
+                elif message[i] == 4:
+                        outsound[i*_len:i*_len+_len] = tone4
+                elif message[i] == 5:
+                        outsound[i*_len:i*_len+_len] = tone5
+                elif message[i] == 6:
+                        outsound[i*_len:i*_len+_len] = tone6
+                elif message[i] == 7:
+                        outsound[i*_len:i*_len+_len] = tone7
+        return outsound
