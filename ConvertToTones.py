8c7233ad1ea4e533cfdcd4c6929fc76cd21d19d1
Grog, the Python Man <Willt125@users.noreply.github.com>
2021-04-09 17:16:07 -0400

    NOW We're Done

--- ConvertToTones.py
+++ ConvertToTones.py
@@ -1,8 +1,20 @@
 import numpy as np
 
+samplerate = 384000
+tone0, tone1, tone2, tone3, tone4, tone5, tone6, tone7 = [], [], [], [], [], [], [], []
+f_c = 1800
+for i in range(160):
+	tone0.append(np.cos(2*np.pi*f_c*i/samplerate))
+	tone1.append(np.cos(2*np.pi*f_c*i/samplerate + 45))
+	tone2.append(np.cos(2*np.pi*f_c*i/samplerate + 90))
+	tone3.append(np.cos(2*np.pi*f_c*i/samplerate + 135))
+	tone4.append(np.cos(2*np.pi*f_c*i/samplerate + 180))
+	tone5.append(np.cos(2*np.pi*f_c*i/samplerate + 225))
+	tone6.append(np.cos(2*np.pi*f_c*i/samplerate + 270))
+	tone7.append(np.cos(2*np.pi*f_c*i/samplerate + 315))
+
+
 def convertToTones(message):
-	# Ah, finally, the last step (almost). This is dead simple.
-	# Had to do it this way because numpy append and concatenate is SO SLOOOW
         outsound = np.empty(len(message) * 160, dtype=np.float32)
         _len = 160
         for i in range(len(message)):
